1d1c8a96822283af46817ea376a880b1ff060ec2
shan wei <swbuild@qq.com>
2018-10-29 14:11:10 +0800

    pom传错了

--- MaxTunnel/MaxTunnel-Common/MaxTunnel-Common-Biz/src/main/java/com/bandweaver/tunnel/common/biz/dto/UserDTO.java
+++ MaxTunnel/MaxTunnel-Common/MaxTunnel-Common-Biz/src/main/java/com/bandweaver/tunnel/common/biz/dto/UserDTO.java
@@ -1,6 +1,98 @@
 package com.bandweaver.tunnel.common.biz.dto;
 
-public class UserDTO {
-    private int ID;
-    private String pwd;
+import com.bandweaver.tunnel.common.biz.pojo.Company;
+import com.bandweaver.tunnel.common.biz.pojo.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.bandweaver.tunnel.common.biz.pojo.Position;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+public class UserDTO implements Serializable{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -6378443201621757910L;
+	private int id;
+    private String name;
+    @JsonIgnore
+    private String password;
+    private Date crtTime;
+    private Set<String> roles;
+    private Set<String> permissions;
+    
+    
+    
+    /**最近一次入廊记录*/
+//    private EntryHistory entryHistory;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User user){
+        if (user == null) return;
+
+        this.id = user.getId();
+        this.name = user.getName();
+    }
+
+    public UserDTO(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    public Set<String> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Set<String> permissions) {
+        this.permissions = permissions;
+    }
+    
+    
+
+    public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+
+	public Date getCrtTime() {
+		return crtTime;
+	}
+
+	public void setCrtTime(Date crtTime) {
+		this.crtTime = crtTime;
+	}
+
+	
 }
--- MaxTunnel/MaxTunnel-Install/src/main/DBScript/create_database.sql
+++ MaxTunnel/MaxTunnel-Install/src/main/DBScript/create_database.sql
@@ -1,34 +1,5 @@
--- Create table
-create table T_USER
-(
-  id       NUMBER not null,
-  username VARCHAR2(30) not null,
-  password VARCHAR2(30)
-)
-tablespace SYSTEM
-  pctfree 10
-  pctused 40
-  initrans 1
-  maxtrans 255
-  storage
-  (
-    initial 64K
-    next 1M
-    minextents 1
-    maxextents unlimited
-  );
--- Create/Recreate primary, unique and foreign key constraints
-alter table T_USER
-  add constraint ID primary key (ID)
-  using index
-  tablespace SYSTEM
-  pctfree 10
-  initrans 2
-  maxtrans 255
-  storage
-  (
-    initial 64K
-    next 1M
-    minextents 1
-    maxextents unlimited
-  );
+@create_database_common.sql
+@create_database_em.sql
+@create_database_mam.sql
+@create_database_oam.sql
+@create_database_omm.sql
