5a8e1d4a928f38b086c662fee85ca3369ac952c5
shan wei <swbuild@qq.com>
2019-01-08 18:16:21 +0800

    修改了vm2中的common和能耗，能耗还要修改

---
+++ MaxTunnel/MaxTunnel-Web/src/main/webapp/WEB-INF/src/components/VM2/Grid1.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class='Grid1'>
+        <div class="Title">
+            <p>{{title}}</p>
+        </div>
+        <div
+            v-for="item in data"
+            class="Row"
+            :key="item.name"
+        >
+            <div class="Name">
+                <p>{{item.name}}</p>
+            </div>
+            <div class="Result">
+                <div class="Value">
+                    <p>{{item.value}}</p>
+                </div>
+                <div class="Unit">
+                    <p>{{item.unit}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Grid1",
+    props: {
+        title: {
+            type: String,
+            require: true
+        },
+        data: {
+            // 传进来的样式
+            // [{
+            //     name: "距离",
+            //     value: 0,
+            //     unit: "km"
+            // }]
+            type: Array,
+            require: true
+        }
+    }
+};
+</script>
+
+<style scoped>
+.Grid1 {
+    position: relative;
+    left: 10%;
+    width: 80%;
+    height: 100%;
+    overflow: auto;
+}
+.Title {
+    width: 100%;
+    height: 20%;
+}
+.Title > p {
+    font-size: 1vmin;
+    color: #fff;
+}
+.Row {
+    width: 100%;
+    height: 15%;
+}
+.Row >>> p {
+    font-size: 0.75vmin;
+    color: #fff;
+}
+.Row .Name {
+    float: left;
+    height: 100%;
+}
+.Row .Result {
+    float: right;
+    height: 100%;
+}
+.Result .Value {
+    float: left;
+    height: 100%;
+}
+.Result .Unit {
+    margin-left: 0.2vw;
+    float: left;
+    height: 100%;
+}
+</style>
