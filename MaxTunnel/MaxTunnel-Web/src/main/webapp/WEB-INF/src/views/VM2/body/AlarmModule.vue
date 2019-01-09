aa281f265f0db74f6ab8427d8e493837d6a86d91
gaoxin <gaoxin@bandweaver.com>
2019-01-09 10:55:53 +0800

    视频，告警，监测

--- MaxTunnel/MaxTunnel-Web/src/main/webapp/WEB-INF/src/views/VM2/body/AlarmModule.vue
+++ MaxTunnel/MaxTunnel-Web/src/main/webapp/WEB-INF/src/views/VM2/body/AlarmModule.vue
@@ -1,13 +1,80 @@
 <template>
     <div class="Main">
-        告警 !!!!
+        <div class="topLeft">
+            <p class="title">告警统计</p>
+            <div class="subTitle">
+                <span>本年度告警：</span>
+                <span class="number">{{ yearTotal.number }}</span>
+                <span class="yearIcon"><Icon :type="yearTotal.isRise ? 'arrow-up-c' : 'arrow-down-c'" :color="yearTotal.isRise ? 'rgb(247, 26, 56)': 'white'"></Icon></span>
+            </div>
+            <div class="subTitle">
+                <span style="letter-spacing: 0.25em;margin-right: -0.25em;">本月告警：</span>
+                <span class="number">{{ monthTotal.number }}</span>
+                <span class="monthIcon"><Icon :type="monthTotal.isRise ? 'arrow-up-c' : 'arrow-down-c'" :color="monthTotal.isRise ? 'rgb(247, 26, 56)': 'white'"></Icon></span>
+            </div>
+        </div>
+        <div class="topRight">
+            <simple-pie-chart v-bind="pieChart"></simple-pie-chart>
+        </div>
+        <div class="bottom">
+             <StackBar ref="stackBar" v-bind="stackBar"></StackBar>
+        </div>
     </div>
 </template>
 
 <script>
+import SimplePieChart from '../../../components/Common/Chart/SimplePieChart'
+import StackBar from "../../../components/Common/Chart/StackChart"
 export default {
     data() {
-        return {};
+        return {
+            yearTotal: {
+                number: 271,
+                isRise: false
+            },
+            monthTotal: {
+                number: 32,
+                isRise: true
+            },
+            pieChart: {
+                requestUrl: "tunnels/items/energies",
+                id: "tunnelEnergyPieChart",
+                parameters: {
+                    option: {
+                        title: {
+                            text: "管廊告警统计",
+                            x: "center",
+                            textStyle: {
+                              fontWeight: "bold",
+                              color: "white",
+                              fontSize: "16"
+                            }
+                        }
+                    }
+                }
+            },
+            stackBar: {
+                id: "stackChart",
+                requestUrl: "stacChart",
+                titleName: "",
+                title: "22",
+                titleColor: "#030303",
+                intervalTime: 1000,
+                tunnelId: null,
+                timeCycle: null,
+                xData: [],
+                legendData: [
+                    "提示",
+                    "一般",
+                    "严重",
+                    "危急"
+                ]
+            },
+        };
+    },
+    components:{
+        SimplePieChart,
+        StackBar
     },
     mounted() {},
     methods: {}
@@ -21,5 +88,56 @@ export default {
     top: 0;
     bottom: 0;
     left: 0;
+    background: url('../../../assets/VM/vm_module_bg.png') no-repeat;
+    background-size: 100% 100%;
 }
+.topLeft{
+    width: 43%;
+    height: 44%;
+    position: absolute;
+    top: 5%;
+    left: 6%;
+}
+.topRight{
+    width: 44%;
+    height: 44%;
+    position: absolute;
+    top: 5%;
+    right: 6%;
+}
+.bottom{
+    position: absolute;
+    width: 90%;
+    height: 42%;
+    top: 50%;
+    right: 6%;
+}
+.title{
+    font-size: 2.4vmin;
+    color: white;
+    margin: 0.8vh 0.8vw 1vh 0.8vw;
+}
+.subTitle{
+    font-size: 1.4vmin;
+    color: white;
+    margin: 0.8vh 0.4vw;
+}
+.number{
+    font-size: 2vmin;
+    margin-left: 0.6vw;
+    color: #37bbcc;
+}
+.yearIcon{
+    font-size: 3vmin;
+    position: absolute;
+    top: 5vh;
+    right: 1.4vw;
+}
+.monthIcon{
+    font-size: 3vmin;
+    position: absolute;
+    top: 8.6vh;
+    right: 1.4vw;
+}
+
 </style>
--- MaxTunnel/MaxTunnel-Web/src/main/webapp/WEB-INF/src/views/VM2/body/MeasureModule.vue
+++ MaxTunnel/MaxTunnel-Web/src/main/webapp/WEB-INF/src/views/VM2/body/MeasureModule.vue
@@ -1,13 +1,68 @@
 <template>
     <div class="Main">
-        监测对象 !!!!
+        <p class="title">监测对象</p>
+        <div class="left">
+            <div class="item" v-for="(item,index) in extreData" :key="index">
+                <p class="name">{{ item.name }}</p>
+                <p class="number">{{ item.number + item.unit }}</p>
+                <p class="location">{{ item.location }}</p>
+            </div>
+        </div>
+        <div class="right">
+            <div class="count" v-for="(item,index) in countData" :key="index">
+                <p class="name">{{ item.name }}</p>
+                <span class="number">{{ item.number + item.unit }}</span>
+                 <span class="riseIcons"><Icon :type="item.isRise ? 'arrow-up-c' : 'arrow-down-c'" :color="item.isRise ? 'rgb(247, 26, 56)': 'white'"></Icon></span>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {};
+        return {
+            extreData: [
+                {
+                    name: '最高温度',
+                    number: 21,
+                    unit: '℃',
+                    location: '古城大街3区污水仓'
+                },
+                {
+                    name: '最高湿度',
+                    number: 22,
+                    unit: 'ppm',
+                    location: '实验路2区综合仓'
+                },
+                {
+                    name: '最低含氧量',
+                    number: 22,
+                    unit: '%',
+                    location: '纬三路1区燃气仓'
+                }
+            ],
+            countData: [
+                {
+                    name: '电子井盖打开',
+                    number: 12,
+                    unit: '次',
+                    isRise: false
+                },
+                {
+                    name: '门禁触发',
+                    number: 15,
+                    unit: '次',
+                    isRise: true
+                },
+                {
+                    name: '红外探测器触发',
+                    number: 2,
+                    unit: '次',
+                    isRise: false
+                }
+            ]
+        };
     },
     mounted() {},
     methods: {}
@@ -21,5 +76,58 @@ export default {
     top: 0;
     bottom: 0;
     left: 0;
+    background: url('../../../assets/VM/vm_module_bg.png') no-repeat;
+    background-size: 100% 100%;
+}
+.title{
+    font-size: 2.4vmin;
+    position: absolute;
+    top: 2.2vh;
+    left: 2vw;
+    color: white;
+}
+.left{
+    position: absolute;
+    width: 44%;
+    height: 73%;
+    top: 18%;
+    left: 7%;
+    overflow: auto;
+}
+.item{
+    background: url('../../../assets/UM/videoBody.png') no-repeat;
+    background-size: 100% 100%;
+    width: 90%;
+    margin: 0.4vh auto;
+    text-align: center;
+}
+.name{
+    color: white;
+    font-size: 1.6vmin;
+}
+.number{
+    color: #37bbcc;
+    font-size: 2vmin;
+}
+.location{
+    color: white;
+    font-size: 1.2vmin;
+}
+.right{
+    width: 40%;
+    height: 73%;
+    position: absolute;
+    top: 18%;
+    right: 8%;
+    text-align: center;
+    overflow: auto;
+}
+.count{
+    margin: 1vh auto;
+}
+.riseIcons{
+    font-size: 2vmin;
+    float: right;
+    margin-right: 1vw;
 }
 </style>
