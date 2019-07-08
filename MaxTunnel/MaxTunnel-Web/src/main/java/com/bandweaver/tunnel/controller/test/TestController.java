d3ca430de26d0249f3c50582c7ced696fa4abc9d
ya.liu <ya.liu@bandweaver.cn>
2019-07-08 16:38:00 +0800

    提交

--- MaxTunnel/MaxTunnel-Web/src/main/java/com/bandweaver/tunnel/controller/test/TestController.java
+++ MaxTunnel/MaxTunnel-Web/src/main/java/com/bandweaver/tunnel/controller/test/TestController.java
@@ -26,6 +26,7 @@ import com.bandweaver.tunnel.common.platform.util.MathUtil;
 import com.bandweaver.tunnel.dao.mam.MeasValueAIMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
