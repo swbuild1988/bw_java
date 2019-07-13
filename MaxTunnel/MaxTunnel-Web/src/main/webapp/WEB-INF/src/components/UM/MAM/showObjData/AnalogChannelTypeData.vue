cd6e89112ded94433ee0bde6bc18365675112df5
gaoxin <gaoxin@bandweaver.com>
2019-07-13 11:49:54 +0800

    img sort and simulate min max data

--- MaxTunnel/MaxTunnel-Web/src/main/webapp/WEB-INF/src/components/UM/MAM/showObjData/AnalogChannelTypeData.vue
+++ MaxTunnel/MaxTunnel-Web/src/main/webapp/WEB-INF/src/components/UM/MAM/showObjData/AnalogChannelTypeData.vue
@@ -112,9 +112,6 @@ export default {
         switchData
     },
     mounted() {
-        // this.sort().then(() => this.transformStateImage());
-        // this.stateSort();
-
         this.transformStateImage();
     },
     methods: {
@@ -136,6 +133,7 @@ export default {
                     }
                 }
             }
+            this.stateSort();
         },
         replaceImage(curStateVal, val, descript, item) {
             // 加载对应状态图片
@@ -185,24 +183,18 @@ export default {
             this.$emit("changeView", id, datatypeId);
         },
         stateSort() {
-            let _this = this;
-            // console.log("dfgdfg");
-            // return new Promise((resolve, reject) => {
-            // for (var j = 0; j < _this.propObj.ObjVal.length - 1; j++) {
-            //     for (var i = 0; i < _this.propObj.ObjVal.length - j - 1; i++) {
-            //         if (
-            //             _this.propObj.ObjVal[i].index >
-            //             _this.propObj.ObjVal[i + 1].index
-            //         ) {
-            //             var temp = _this.propObj.ObjVal[i];
-            //             _this.propObj.ObjVal[i] = _this.propObj.ObjVal[i + 1];
-            //             _this.propObj.ObjVal[i + 1] = temp;
-            //         }
-            //     }
-            // }
-            console.log(this.curObjState);
-            //     resolve();
-            // });
+            for (var j = 0; j < this.curObjState.length - 1; j++) {
+                for (var i = 0; i < this.curObjState.length - j - 1; i++) {
+                    if (
+                        this.curObjState[i].index >
+                        this.curObjState[i + 1].index
+                    ) {
+                        var temp = this.curObjState[i];
+                        this.curObjState[i] = this.curObjState[i + 1];
+                        this.curObjState[i + 1] = temp;
+                    }
+                }
+            }
         }
     }
 };
